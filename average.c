7d8e96641eb1e02cd9310d918a6b014eaf0e41f6
nivethaaarthi98 <30368440+nivethaaarthi98@users.noreply.github.com>
2017-07-25 11:57:57 +0530

    Create average.c

---
+++ average.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+int
+ar[100],n,i,sum=0;
+float avg;
+printf("enter the size of the array");
+scanf("%d",&n);
+printf("enter the elements of the array to find their average");
+for(i=0;i<n;i++)
+{
+scanf("%d",&ar[i]);
+sum=sum+ar[i]);
+}
+avg=float)sum/n;
+printf("average is %f",avg);
+getch();
+}
